3b80026f3a0d89d667c1f1015c6384e1e3d76aa2
Alejandro Diez Isaza <alejandro.diez@ceiba.com.co>
2020-10-14 16:13:36 -0500

    add validators kardex

--- src/app/shared/components/kardex/kardex.component.ts
+++ src/app/shared/components/kardex/kardex.component.ts
@@ -1,29 +1,33 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { CrudService } from '@app/core';
-import { forkJoin, Observable, of } from 'rxjs';
+import { forkJoin, Observable, of, Subscription } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { Kardex } from './../../models/kardex.model';
 import { Module } from './../../models/module.model';
 import { Provider } from './../../models/provider.model';
 import { Customer } from './../../models/customer.model';
 import { Game } from './../../models/game.model';
+import { decimalNumber, onlyNumber } from './../../constants/pattern.constants';
 
 @Component({
   selector: 'app-kardex',
   templateUrl: './kardex.component.html',
   styleUrls: ['./kardex.component.css']
 })
-export class KardexComponent implements OnInit {
+export class KardexComponent implements OnInit, OnDestroy {
 
   @Input() id: number;
   @Input() module: Module;
   @Output() createOrUpdate = new EventEmitter<Kardex>();
 
+  private unsubscribe: Subscription;
+
   form: FormGroup;
   provider: Provider[];
   customer: Customer[];
   game: Game[];
+  isEntry: boolean;
   ready = false;
 
   constructor(
@@ -70,19 +74,58 @@ export class KardexComponent implements OnInit {
   }
 
   private createForm(kardex?: Kardex): void {
+    const dateTransaction = (kardex?.transaction)
+      ? new Date(kardex.transaction) : null;
+    const entryOrExit = (kardex?.entryOrExit)
+      ? kardex.entryOrExit : true;
+
     this.form = new FormGroup({
-      transaction: new FormControl(null, []),
-      entryOrExit: new FormControl(null, []),
-      quantity: new FormControl(null, []),
-      price: new FormControl(null, []),
-      provider: new FormControl(null, []),
-      customer: new FormControl(null, []),
-      game: new FormControl(null, [])
+      transaction: new FormControl(dateTransaction, [Validators.required]),
+      entryOrExit: new FormControl(entryOrExit, [Validators.required]),
+      quantity: new FormControl(kardex?.quantity, [Validators.required, Validators.pattern(onlyNumber)]),
+      price: new FormControl(kardex?.price, [Validators.required, Validators.pattern(decimalNumber)]),
+      provider: new FormControl(kardex?.provider?.id, []),
+      customer: new FormControl(kardex?.customer?.id, []),
+      game: new FormControl(kardex?.game?.id, [Validators.required])
     });
+
+    this.unsubscribe = this.form.get('entryOrExit')
+      .valueChanges
+      .subscribe((isEntry: boolean) => this.validateInput(isEntry));
+
+    this.validateInput(entryOrExit);
+  }
+
+  private validateInput(isEntry: boolean): void {
+    const provider = this.form.get('provider');
+    const customer = this.form.get('customer');
+    this.isEntry = isEntry;
+
+    if (isEntry) {
+      provider.setValidators([Validators.required]);
+      customer.setValidators([]);
+    } else {
+      customer.setValidators([Validators.required]);
+      provider.setValidators([]);
+    }
+
+    provider.updateValueAndValidity();
+    customer.updateValueAndValidity();
   }
 
   save(): void {
-    this.createOrUpdate.emit(this.form.getRawValue());
+    (this.form.valid)
+      ? this.createOrUpdate.emit({
+        ...this.form.getRawValue(),
+        id: this.id
+      })
+      : this.form.markAllAsTouched();
+  }
+
+  ngOnDestroy(): void {
+    if (this.unsubscribe) {
+      this.unsubscribe.unsubscribe();
+    }
   }
 
 }
