ca88a19ef4990c28ed106da3d1770a86f797d035
Alejandro Diez Isaza <alejandro.diez@ceiba.com.co>
2020-10-19 15:31:12 -0500

    create test protractor

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,16 +1,65 @@
 import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import { browser, by, element, logging } from 'protractor';
 
-describe('Start App', () => {
-  let page: AppPage;
+describe('When starting the application', () => {
 
-  beforeEach(() => {
-    page = new AppPage();
+  const page = new AppPage();
+
+  describe('The AppModule is compiled', () => {
+
+    beforeAll(() => page.navigateTo());
+
+    it('Should redirect to login', () => {
+      const title = element(by.css('#title-app')).getText();
+      expect(title).toEqual('ADI Gamer');
+    });
+
+    it('Should create the form', () => {
+      const inputDocument = element(by.css('input[formcontrolname="document"]'));
+      const inputPassword = element(by.css('input[formcontrolname="password"]'));
+      const btnLogin = element(by.css('#btn-login'));
+
+      expect(inputDocument).toBeDefined();
+      expect(inputPassword).toBeDefined();
+      expect(btnLogin).toBeDefined();
+    });
   });
 
-  it('Show login title', () => {
-    page.navigateTo();
-    expect(page.getTitleText('#title-app')).toEqual('ADI Gamer');
+  describe('The User login', () => {
+
+    it('Should must ask for the mandatory data', () => {
+      const btnLogin = element(by.css('#btn-login'));
+      btnLogin.click();
+
+      const errorDocument = element(by.css('#err-document-requerid'));
+      const errorPassword = element(by.css('#err-document-requerid'));
+      expect(errorDocument.getText()).toEqual('El campo es obligatorio');
+      expect(errorPassword.getText()).toEqual('El campo es obligatorio');
+    });
+
+    it('Should show data error', () => {
+      const inputDocument = element(by.css('input[formcontrolname="document"]'));
+      const inputPassword = element(by.css('input[formcontrolname="password"]'));
+      const btnLogin = element(by.css('#btn-login'));
+      inputDocument.value = '1017249864';
+      inputPassword.value = '2222221';
+      btnLogin.click();
+
+      const errorData = element(by.css('#error-data'));
+      expect(errorData).toBeDefined();
+    });
+
+    it('Should log in', () => {
+      const inputDocument = element(by.css('input[formcontrolname="document"]'));
+      const inputPassword = element(by.css('input[formcontrolname="password"]'));
+      const btnLogin = element(by.css('#btn-login'));
+      inputDocument.value = '1017249864';
+      inputPassword.value = '2222222';
+      btnLogin.click();
+
+      const navbar = element(by.css('#navbar-app'));
+      expect(navbar).toBeDefined();
+    });
   });
 
   afterEach(async () => {
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -4,8 +4,4 @@ export class AppPage {
   navigateTo(url = ''): Promise<unknown> {
     return browser.get(`${browser.baseUrl}${url}`) as Promise<unknown>;
   }
-
-  getTitleText(id: string): Promise<string> {
-    return element(by.css(id)).getText() as Promise<string>;
-  }
 }
